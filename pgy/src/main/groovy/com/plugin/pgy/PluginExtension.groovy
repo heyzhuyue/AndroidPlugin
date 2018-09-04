13ce6063a5fd2c9271c0db6881975ca76f032d94
heyzhuyue <1301529626@qq.com>
2018-09-04 14:03:56 +0800

    - 蒲公英上传插件修改

--- pgy/src/main/groovy/com/plugin/pgy/PluginExtension.groovy
+++ pgy/src/main/groovy/com/plugin/pgy/PluginExtension.groovy
@@ -5,18 +5,11 @@ class PluginExtension {
     String sourceApk
     String _api_key
     String desc
-    String ceshiMobile
-    String kaifaMobile
-    String access_token
 
     PluginExtension() {
         sourceApk = ''
         _api_key = ''
         desc = ''
-        ceshiMobile = ''
-        kaifaMobile = ''
-        access_token = ''
-
     }
 
 }
